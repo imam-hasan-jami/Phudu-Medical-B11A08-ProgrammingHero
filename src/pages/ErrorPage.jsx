b773e0a3454832c765af9ccd21ad00f209f4f473
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-22 14:44:11 +0600

    designed the error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { Link } from 'react-router';
 
 const ErrorPage = () => {
     return (
-        <div>
+        <div className='max-w-[380px] mx-auto lg:max-w-[1440px] min-h-screen'>
             <Navbar />
-            <h1>Error 404 Not Found</h1>
+            <div className='flex flex-col justify-center items-center mt-35'>
+                <p className='text-2xl text-[#176AE5] font-semibold'>OOPS...</p>
+                <h1 className='text-[180px] text-[#176AE5] font-bold mt-[-40px]'>404</h1>
+                <h1 className='text-[40px] text-[#176AE5] font-bold mt-[-30px]'>Page Not Found</h1>
+                <Link to={"/"}>
+                    <button className='bg-[#176AE5] text-white text-2xl px-5 py-3 rounded-[99px] mt-14 cursor-pointer'>Back to Home</button>
+                </Link>
+            </div>
         </div>
     );
 };
