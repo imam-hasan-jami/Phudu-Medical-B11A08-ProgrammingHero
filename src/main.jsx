e0198e2fa740e401055cbf7a63a6b148a965536f
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-23 13:10:07 +0600

    installed and setup react-hot-toast

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import router from './routes/routes.jsx'
 import BlogsProvider from './providers/BlogsProvider.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BlogsProvider>
       <RouterProvider router={router} />
+      <Toaster position='top-right' />
     </BlogsProvider>
   </StrictMode>,
 )
